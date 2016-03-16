61fc7544a4f5af9dbdbf6fa30678323ee9503cdf
kjuen <Klaus.Juenemann@haw-hamburg.de>
2016-03-16 08:25:57 +0100

    LGSViewer verbessert

--- LGSViewer/LGSViewer.js
+++ LGSViewer/LGSViewer.js
@@ -1,228 +1,378 @@
-/*global addWorldAxes, dat, THREE */
+/*global addWorldAxes, dat, THREE, getPlaneCorners, lgsCon, calcIntersectionLine */
+
+// TODO: Mit Plane2 rumspielen, ob das besser aussieht
 
 //* Initialize webGL
-var canvas = document.getElementById("mycanvas");
-var renderer = new THREE.WebGLRenderer({canvas:canvas});
-renderer.setClearColor('rgb(255,255,255)');    // set background color
+var renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+// var canvas = document.getElementById("mycanvas");
+// var renderer = new THREE.WebGLRenderer({canvas:canvas});
+renderer.setClearColor('white');    // set background color
 
 // Create a new Three.js scene and a camera
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height,
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight,
                                           0.1, 1000 );
-camera.position.set(8,8,8);
+camera.position.set(12,12,12);
+window.addEventListener('resize', function() {
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.aspect= window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}, false);
 
-//* Data management and dat.gui
-var lgsCon = {
-  a11: 1,
-  a12: 0.1,
-  a13: 0,
-  b1: 1,
-  a21: 0,
-  a22: 1,
-  a23: 0,
-  b2: 1,
-  a31: 0,
-  a32: 0,
-  a33: 1,
-  b3: 1,
-  getNormal: function(idx) {
-    if(idx===1) {
-      return new THREE.Vector3(this.a11, this.a12, this.a13);
-    } else if(idx===2) {
-      return new THREE.Vector3(this.a21, this.a22, this.a23);
-    } else {
-      return new THREE.Vector3(this.a31, this.a32, this.a33);
-    }
-  },
-  getB: function(idx) {
-    if(idx===1) return this.b1;
-    else if(idx===2) return this.b2;
-    else return this.b3;
-  },
 
-  Normalenvektoren: false,
-  Transparenz:1
+// In render loop, position is set to camera position
+var light = new THREE.PointLight( 0xffffff );
+scene.add( light );
+scene.add(new THREE.AmbientLight(0xaaaaaa));
+
+
+
+//* General helper functions
+var makeTextLabel = function( message, fontsize, col )
+{
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  context.font = "Bold " + fontsize + "px Arial";
+
+  // get size data (height depends only on font size)
+  // var metrics = context.measureText( message );
+  //var textWidth = metrics.width;
+  context.fillStyle = col.getStyle(); // "rgb(0,0,0)";
+  context.fillText( message, fontsize, fontsize);
+
+  // canvas contents will be used for a texture
+  var texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+  var spriteMaterial = new THREE.SpriteMaterial({ map: texture});
+  var sprite = new THREE.Sprite( spriteMaterial );
+  return sprite;
 };
 
+
+/**
+ * create an arrow pointing into y direction
+ */
+function createTubeArrow(opts) {
+  // set defaults
+  opts = opts || {};
+  opts.len = opts.len || 1;
+  opts.thick = opts.thick || 1/50;
+  opts.col = opts.col || new THREE.Color('darkslategray');
+
+  var mat = new THREE.MeshPhongMaterial({color: opts.col,
+                                         specular: opts.col,
+                                         side:THREE.DoubleSide});
+  var tubeGeo = new THREE.CylinderGeometry(opts.thick, opts.thick, opts.len, 32);
+  var tube = new THREE.Mesh(tubeGeo, mat);
+  tube.position.y = opts.len/2;
+  var headGeo = new THREE.CylinderGeometry(0, 3*opts.thick, 6*opts.thick, 32);
+  var head = new THREE.Mesh(headGeo, mat);
+  head.position.y = opts.len+opts.thick;
+  var axis = new THREE.Object3D();
+  axis.add(tube);
+  axis.add(head);
+  if(opts.label !== undefined) {
+    opts.labelSize = opts.labelSize || 48;
+    var sprite = makeTextLabel(opts.label, opts.labelSize, opts.col);
+    sprite.position.y = opts.len;
+    axis.add( sprite );
+  }
+  return axis;
+}
+
+
+
+//* Data management and dat.gui
 
 // FIXME: Die Schrittweite wird nicht angezeigt bei Elementen mit Startwert 0!!
 window.onload = function() {
   var gui = new dat.GUI();
-  var eq1 = gui.addFolder('Gleichung 1');
-  var a11 = eq1.add(lgsCon, 'a11', -3,3).step(0.1);
-  a11.onChange(function() { setLocation(plane1, 1, ar1); });
-  var a12 = eq1.add(lgsCon, 'a12', -3,3).step(0.1);
-  a12.onChange(function() { setLocation(plane1, 1, ar1); });
-  var a13 = eq1.add(lgsCon, 'a13', -3,3).step(0.1);
-  a13.onChange(function() { setLocation(plane1, 1, ar1); });
-  var b1 = eq1.add(lgsCon, 'b1', -3,3).step(0.1);
-  b1.onChange(function() { setLocation(plane1, 1, ar1); });
-  var eq2 = gui.addFolder('Gleichung 2');
-  var a21 = eq2.add(lgsCon, 'a21', -3,3).step(0.1);
-  a21.onChange(function() { setLocation(plane2, 2, ar2); });
-  var a22 = eq2.add(lgsCon, 'a22', -3,3).step(0.1);
-  a22.onChange(function() { setLocation(plane2, 2, ar2); });
-  var a23 = eq2.add(lgsCon, 'a23', -3,3).step(0.1);
-  a23.onChange(function() { setLocation(plane2, 2, ar2); });
-  var b2 = eq2.add(lgsCon, 'b2', -3,3).step(0.1);
-  b2.onChange(function() { setLocation(plane2, 2, ar2); });
-  var eq3 = gui.addFolder('Gleichung 3');
-  var a31 = eq3.add(lgsCon, 'a31', -3,3).step(0.1);
-  a31.onChange(function() { setLocation(plane3, 3, ar3); });
-  var a32 = eq3.add(lgsCon, 'a32', -3,3).step(0.1);
-  a32.onChange(function() { setLocation(plane3, 3, ar3); });
-  var a33 = eq3.add(lgsCon, 'a33', -3,3).step(0.1);
-  a33.onChange(function() { setLocation(plane3, 3, ar3); });
-  var b3 = eq3.add(lgsCon, 'b3', -3,3).step(0.1);
-  b3.onChange(function() { setLocation(plane3, 3, ar3); });
+  var cb = function() {
+    // planes[this].setKoeffs(lgsCon.eqs[this].getA(), lgsCon.eqs[this].getB());
+    planes[this].update();
+    var solpos = lgsCon.getSolution();
+    if(solpos === undefined) {
+      sol.visible = false;
+    } else {
+      sol.visible = true;
+      sol.position.copy(lgsCon.getSolution());
+    }
+    lines.forEach(function(l) { l.update(); });
+  };
+  for(var ii=1;ii<=3;ii++) {
+    var cbii = cb.bind(ii-1);
+    var eq = gui.addFolder('Ebene ' + ii);
+    for(var j=1;j<=3;j++) {
+      eq.add(lgsCon.eqs[ii-1], 'a'+ii+j, -3,3).step(0.1).onChange(cbii);
+    }
+    eq.add(lgsCon.eqs[ii-1], 'b'+ii, -3,3).step(0.1).onChange(cbii);
+  }
+
   var einst = gui.addFolder('Einstellungen');
-  einst.add(lgsCon, 'Transparenz', 0,1).onChange(
+  einst.add(lgsCon, 'Transparenz', 0, 1).onChange(
     function(tr) {
-      plane1.material.opacity = tr;
-      plane2.material.opacity = tr;
-      plane3.material.opacity = tr;
+      planes.forEach( function(pl) {
+        if(tr < lgsCon.TranspTheshold) {
+          pl.mesh.visible = false;
+        } else {
+          pl.mesh.visible = true;
+          pl.mesh.traverse(function(obj) {
+            if(obj.material !== undefined)
+              obj.material.opacity = tr;
+          });
+        }
+      });
     });
 
+  einst.add(lgsCon, 'Normalenvektoren').onChange(
+    function(flag) {
+      planes.forEach( function(pl) {
+        pl.normal.visible = flag;
+      });
+    });
+  einst.add(lgsCon, 'Achsenkreuz').onChange(
+    function(flag) {
+      worldAxes.visible = flag;
+    });
 };
-//* Draw the planes
-var showPlaneNormals = true;
-var planeSize = 5;
-var planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-var planeCol1 = 0x505000;
-var planeCol2 = 0x005050;
-var planeCol3 = 0x500050;
-var planeMat = new THREE.MeshBasicMaterial( {color: planeCol3, side: THREE.DoubleSide} );
-
-function createPlane(col, size) {
-  size = (size === undefined ?  5 : size);
-
-  var geometry = new THREE.PlaneGeometry(size, size);
-  var material = new THREE.MeshBasicMaterial( {color: new THREE.Color(col),
-                                               side: THREE.DoubleSide} );
-  material.transparent = true;
-  material.opacity = lgsCon.Transparenz;
-  var plane = new THREE.Mesh( geometry, material );
-
-  return plane;
-}
 
 
-function createPlaneNormal(col) {
-  var pos = new THREE.Vector3(0,0,0);
-  var dir = new THREE.Vector3(1,0,0);
-  return new THREE.ArrowHelper(dir, pos, 1, col);
-}
 
-// FIXME: Stimmt der Code hier wirklich? Reihenfolge von Rotation und Translation
-//        sind nicht richtig, glaube ich
+//* Planes
 
 /**
- * set the location (position and rotation) of the plane so that the points on
- * the plane satisfy the equation a*x=b.
- * @param{Object} plane the plane to locate
- * @param{number} idx index of equation
- * @param{Object} ar optional ArrowHelper object showing the plane normal
+ * returns the plane geometry or undefined if there are less than 3 vertices
  */
-function setLocation(plane, idx, ar) {
+function createPlaneGeo(a,b,L) {
 
-  var TOL = 0.01;
-  var a = lgsCon.getNormal(idx);
-  var b = lgsCon.getB(idx);
+  var geo = new THREE.Geometry();
+  geo.vertices = getPlaneCorners(a,b,L);
+  if(geo.vertices === undefined || geo.vertices.length < 3) {
+    return undefined;
+  }
+  for(var i = 1; i<=geo.vertices.length-2; ++i)
+    geo.faces.push(new THREE.Face3(0,i,i+1));  // 0
+  // geo.faces.push(new THREE.Face3(1,2,3));  // 1
 
-  var len = a.length();
-  if(len<TOL) {
-    plane.visible = false;
-  } else {
-    plane.visible = true;
-    var angle = Math.acos(a.z/a.length());
-    var norm = Math.sqrt(a.x*a.x+a.y*a.y);
-    var axis = new THREE.Vector3(a.y/norm, -a.x/norm, 0);
-    var q = new THREE.Quaternion();
-    q.setFromAxisAngle ( axis, angle );
-    plane.quaternion.copy(q);
-    a.multiplyScalar(b/(len*len));
-    plane.position.copy(a);
+  geo.computeFaceNormals();
+  //geo.computeVertexNormals();  // no good
+  geo.faces.forEach(function(f) {
+    f.vertexNormals.push(f.normal.clone());
+    f.vertexNormals.push(f.normal.clone());
+    f.vertexNormals.push(f.normal.clone());
+  });
 
-    if(showPlaneNormals && ar !== undefined) {
-      ar.position.copy(a);
-      ar.setDirection(a.normalize());
+  geo.dynamic = true;
+  geo.verticesNeedUpdate = true;
+  geo.normalsNeedUpdate = true;
+  return geo;
+}
+
+
+/**
+ * Position normal vector to plane a*x=b
+ */
+function positionNormal(normal, a, b) {
+  normal.position.copy(a.clone().multiplyScalar(b/a.lengthSq()));
+  var m = new THREE.Matrix4();
+  var theta = Math.acos(a.y/a.length());
+  m.makeRotationAxis(new THREE.Vector3(a.z, 0, -a.x).normalize(), theta);
+  normal.rotation.setFromRotationMatrix(m);
+}
+
+
+function Plane(idx) {
+  this.idx = idx;
+  var a = lgsCon.eqs[idx].getA();
+  var b = lgsCon.eqs[idx].getB();
+
+  // var geometry = new THREE.PlaneBufferGeometry(size, size);
+  var geometry = createPlaneGeo(a, b, lgsCon.planeSize);
+  var material = new THREE.MeshPhongMaterial( {color: lgsCon.eqs[idx].col,
+                                               specular: lgsCon.eqs[idx].col,
+                                               side: THREE.DoubleSide} );
+  this.mesh = new THREE.Mesh( geometry, material );
+  this.mesh.matrixAutoUpdate = false;
+  scene.add(this.mesh);
+
+  this.normal = createTubeArrow({len:1, col:lgsCon.eqs[idx].col});
+  positionNormal(this.normal, a, b);
+  this.normal.visible = lgsCon.Normalenvektoren;
+  this.mesh.add(this.normal);
+  this.mesh.traverse(function(obj) {
+    if(obj.material !== undefined) {
+      obj.material.transparent = true;
+      obj.material.opacity = lgsCon.Transparenz;
     }
+  });
+}
+
+
+
+
+
+Plane.prototype.update = function() {
+  var a = lgsCon.eqs[this.idx].getA();
+  var b = lgsCon.eqs[this.idx].getB();
+  var geo = createPlaneGeo(a, b, lgsCon.planeSize);
+  if(geo !== undefined) {
+    this.mesh.geometry = geo;
+    positionNormal(this.normal, a, b);
+  }
+};
+
+Plane.prototype.setVisible = function(flag) {
+  if(flag && lgsCon.Transparenz>=lgsCon.TranspTheshold) {
+    this.mesh.visible = true;
+  } else {
+    this.mesh.visible = false;
+  }
+};
+
+
+function Plane2(idx) {
+  this.idx = idx;
+  var a = lgsCon.eqs[idx].getA();
+  var b = lgsCon.eqs[idx].getB();
+
+  // this create plane in x-y-plane
+  var geometry = new THREE.PlaneBufferGeometry(lgsCon.planeSize, lgsCon.planeSize);
+  var material = new THREE.MeshPhongMaterial( {color: lgsCon.eqs[idx].col,
+                                               specular: lgsCon.eqs[idx].col,
+                                               side: THREE.DoubleSide} );
+  this.mesh = new THREE.Mesh( geometry, material );
+  scene.add(this.mesh);
+
+  this.mesh.position.copy(a.clone().multiplyScalar(b/a.lengthSq()));
+  var m = new THREE.Matrix4();
+  var theta = Math.acos(a.z/a.length());
+  m.makeRotationAxis(new THREE.Vector3(a.y, -a.x, 0).normalize(), theta);
+  this.mesh.rotation.setFromRotationMatrix(m);
+
+  this.normal = createTubeArrow({len:1, col:lgsCon.eqs[idx].col});
+  positionNormal(this.normal, a, b);
+  this.normal.visible = lgsCon.Normalenvektoren;
+  this.mesh.add(this.normal);
+  this.mesh.traverse(function(obj) {
+    if(obj.material !== undefined) {
+      obj.material.transparent = true;
+      obj.material.opacity = lgsCon.Transparenz;
+    }
+  });
+}
+
+
+
+//** Draw the planes
+var planes = new Array(3);
+planes[0] = new Plane(0);
+planes[1] = new Plane(1);
+planes[2] = new Plane(2);
+
+var p0 = new Plane2(0);
+var p1 = new Plane2(1);
+var p2 = new Plane2(2);
+
+//** Draw solution as black sphere
+var sol = new THREE.Mesh(new THREE.SphereGeometry (0.1, 16, 16),
+                         new THREE.MeshPhongMaterial({color:0x202020,
+                                                      specular:0x303030,
+                                                      shininess:5}));
+sol.position.copy(lgsCon.getSolution());
+scene.add(sol);
+
+
+//* Create line for testing
+
+function IntersectionLine(idx1, idx2) {
+  this.idx1 = idx1;
+  this.idx2 = idx2;
+  var endPoints = calcIntersectionLine(lgsCon.eqs[idx1].getA(), lgsCon.eqs[idx1].getB(),
+                                       lgsCon.eqs[idx2].getA(), lgsCon.eqs[idx2].getB(),
+                                       lgsCon.planeSize);
+  if(endPoints.length == 2) {
+    var tg = new THREE.TubeGeometry(new THREE.LineCurve3(endPoints[0], endPoints[1]),
+                                    2, 0.05, 16, false);
+    var col = lgsCon.eqs[idx1].col.clone().add(lgsCon.eqs[idx2].col);
+
+    this.mesh = new THREE.Mesh(tg, new THREE.MeshPhongMaterial({color:col,
+                                                                side:THREE.DoubleSide,
+                                                                specular:0xffff00,
+                                                                wireframe:false}));
+    scene.add(this.mesh);
   }
 }
 
+IntersectionLine.prototype.update = function() {
+  var endPoints = calcIntersectionLine(lgsCon.eqs[this.idx1].getA(), lgsCon.eqs[this.idx1].getB(),
+                                       lgsCon.eqs[this.idx2].getA(), lgsCon.eqs[this.idx2].getB(),
+                                       lgsCon.planeSize);
+  if(endPoints.length != 2) return;
+  var tg = new THREE.TubeGeometry(new THREE.LineCurve3(endPoints[0], endPoints[1]),
+                                  2, 0.05, 16, false);
+  this.mesh.geometry = tg;
+};
+
+var lines = new Array(3);
+lines[0] = new IntersectionLine(0,1);
+lines[1] = new IntersectionLine(0,2);
+lines[2] = new IntersectionLine(1,2);
 
 
-
-var plane1 = createPlane('rgb(200, 200, 0)');
-scene.add( plane1 );
-var plane2 = createPlane('rgb(200, 0, 200)');
-scene.add( plane2 );
-var plane3 = createPlane('rgb(0, 200, 200)');
-scene.add( plane3 );
-if(showPlaneNormals) {
-  var ar1 = createPlaneNormal(planeCol1);
-  scene.add(ar1);
-  var ar2 = createPlaneNormal(planeCol2);
-  scene.add(ar2);
-  var ar3 = createPlaneNormal(planeCol3);
-  scene.add(ar3);
-}
-
-
+//** Draw the lines
 
 
 //* Add x,y,z coordinate axes to scene
-function addWorldAxes(scene, len, thick) {
 
-  if(len===undefined) len = 1.5;
+function createWorldAxes(scene, len, thick) {
+
+  if(len===undefined) len = 3;
   if(thick===undefined) thick = 1/50;
-  var greenMat = new THREE.MeshBasicMaterial({color: 'green'});
-  var axisGeo = new THREE.CylinderGeometry(thick, thick, len, 48);
-  var axis = new THREE.Mesh(axisGeo, greenMat);
-  var headGeo = new THREE.CylinderGeometry(0, 2*thick, 2*thick, 48);
-  var head = new THREE.Mesh(headGeo, greenMat);
-  head.position.y = len/2+thick;
-  var yAxis = new THREE.Object3D();
-  yAxis.add(axis);
-  yAxis.add(head);
-  yAxis.position.y=len/6;
-  scene.add(yAxis);
+  var labelSize = 48;
+  var col = new THREE.Color(0x303030);
 
+  var worldAxes = new THREE.Object3D();
+  var yAxis = createTubeArrow({len: len, thick: thick, col: col,
+                               label: 'y', labelSize: labelSize});
+  yAxis.position.y=-len/3;
+  worldAxes.add(yAxis);
 
-  var blueMat = new THREE.MeshBasicMaterial({color: 'blue'});
-  axis = new THREE.Mesh(axisGeo, blueMat);
-  head = new THREE.Mesh(headGeo, blueMat);
-  head.position.y = len/2+thick;
-  var zAxis = new THREE.Object3D();
-  zAxis.add(axis);
-  zAxis.add(head);
+  var zAxis = createTubeArrow({len: len, thick: thick, col: col,
+                               label: 'z', labelSize: labelSize});
   zAxis.rotation.x = Math.PI/2;
-  zAxis.position.z = len/6;
-  scene.add(zAxis);
+  zAxis.position.z = -len/3;
+  worldAxes.add(zAxis);
 
-  var redMat = new THREE.MeshBasicMaterial({color: 'red'});
-  axis = new THREE.Mesh(axisGeo, redMat);
-  head = new THREE.Mesh(headGeo, redMat);
-  head.position.y = len/2+thick;
-  var xAxis = new THREE.Object3D();
-  xAxis.add(axis);
-  xAxis.add(head);
+  var xAxis = createTubeArrow({len: len, thick: thick, col: col,
+                               label: 'x', labelSize: labelSize});
   xAxis.rotation.z = -Math.PI/2;
-  xAxis.position.x = len/6;
-  scene.add(xAxis);
+  xAxis.position.x = -len/3;
+  worldAxes.add(xAxis);
+  return worldAxes;
 }
-addWorldAxes(scene);
+var worldAxes = createWorldAxes();
+worldAxes.visible = lgsCon.Achsenkreuz;
+scene.add(worldAxes);
+
+
 
 //* Rendering
 
-setLocation(plane1, 1, ar1);
-setLocation(plane2, 2, ar2);
-setLocation(plane3, 3, ar3);
+// var outerBox = new THREE.Mesh(new THREE.BoxGeometry(2*lgsCon.planeSize, 2*lgsCon.planeSize, 2*lgsCon.planeSize),
+//                               new THREE.MeshPhongMaterial({color:'black',
+//                                                            transparent:true,
+//                                                            opacity:0.3}));
+// var boxEdges = new THREE.EdgesHelper( outerBox, 0x000000 );
+// scene.add( boxEdges );
 
-var controls = new THREE.OrbitControls( camera, canvas );
+
+var controls = new THREE.OrbitControls( camera );
 function render() {
   requestAnimationFrame(render);
 
+  light.position.copy(camera.position);
 
   controls.update();
   renderer.render(scene, camera);
